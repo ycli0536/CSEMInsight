8c4744324ca658bbe7b5ccf38cf48085dcfabe80
yli3354 <yli0536@gatech.edu>
2024-10-30 17:47:52 -0400

    feat: custom floating filters for Freq#, Rx#, and Tx# in data table
    - Add @ag-grid-community/react package

---
+++ frontend/src/components/custom/numberFloatingFilterComponent.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, Fragment } from 'react';
+import { CustomFloatingFilterProps } from '@ag-grid-community/react';
+import { Input } from '@/components/ui/input'; // Import shadcn/ui Input
+
+
+const NumberFloatingFilterComponent = ({ model, onModelChange }: CustomFloatingFilterProps) => {
+    const value = (model && model.filter) || '';
+
+    const onInput = ({ target: { value: newValue } }: ChangeEvent<HTMLInputElement>) => {
+        onModelChange(
+            newValue === ''
+                ? null
+                : {
+                      ...(model || {
+                          type: 'equals',
+                      }),
+                      filter: Number(newValue),
+                  }
+        );
+    };
+    return (
+        <div className="py-1.5">
+            <Fragment>
+                <Input
+                className='h-full py-0 ring-offset-0 ring-2 ring-inset focus-visible:ring-offset-0'
+                value={value} 
+                type="number" 
+                min="0" 
+                onInput={onInput}
+                />
+            </Fragment>
+        </div>
+    );
+};
+
+export default NumberFloatingFilterComponent;
